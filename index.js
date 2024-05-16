8ebefed099dccd98d3ef173cdede6841283422a3
fabriceMokfembam <fabricemokfembam@.com>
2024-05-16 12:21:25 -0700

    completed saving and retriving

--- index.js
+++ index.js
@@ -17,56 +17,73 @@ app.use(cors(corsOptions));
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-group", (name) => {
+  socket.on("join-group", async (name) => {
     socket.join(name);
-    const document = findOrCreateDocument(name);
-    io.to(name).emit("load-document", document.textContent);
     console.log("joined group", socket.id);
-
+     
+    const newDoc = {
+    name,
+    textContent: '', 
+  };
+    const docs = await Document.findOne({ name }); 
+    const loadData = Loader(docs);
+    if (loadData === '') {
+      const created = await Document.create(newDoc);
+      console.log('created', created);
+    } else {
+      socket.emit('load-document', loadData);        
+      console.log(loadData);  
+    }  
+    
+ 
     socket.on("send-words", (words) => {
       io.to(name).emit("received-words", words);
-      saveDocument(name, words); // Autosave the document
-    });
+    
+    });  
+
+    socket.on('save-documents', async (data) => {
+      const saved = await Document.findOneAndUpdate({ name }, { textContent: data }, { new: true });
+      console.log(saved);
+    })
   });
 });
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {  
   res.send("yo");
-});
+});    
 
-async function findOrCreateDocument(name) {
-  if (!name) return;
+function Loader(docs) { 
+  if (docs) return docs.textContent;
 
-  const document = await Document.findOne({ name });
-  if (document) return document;
-  return await Document.create({ name, textContent: "" });
-}
+  return '';
+      
+} 
 
-async function saveDocument(name, textContent) {
-  try {
-    const document = await Document.findOneAndUpdate(
-      { name },
-      { textContent },
-      { new: true }
-    );
-    console.log("Document saved:", document);
-  } catch (error) {
-    console.log("Error saving document:", error);
-  }
-}
+// async function saveDocument(name, textContent) {
+//   try {
+//     const document = await Document.findOneAndUpdate(
+//       { name },
+//       { textContent },
+//       { new: true }
+//     );
+//     console.log("Document saved:", document);
+//   } catch (error) {
+//     console.log("Error saving document:", error);
+//   }
+// }
 
 const connection = async () => {
   try {
     await mongoose.connect(
       `mongodb+srv://ThiagoFabrice:Thiago+123.@blogcluster.oorq86r.mongodb.net/documents`
     );
-    console.log("connected to db");
+    console.log("connected to db"); 
   } catch (error) {
     console.log("error connecting to db", error);
   }
